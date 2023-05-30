a2a1d59a8a4314f484504adf1243dfbd39533159
joejamsky <joejamsky@gmail.com>
2023-05-29 21:40:42 -0400

    test textures and add build files

--- VRshooter/gulpfile.js
+++ VRshooter/gulpfile.js
@@ -1,5 +1,5 @@
-const { src, dest, series, watch} = require('gulp');
-
+const { src, dest, series, watch } = require('gulp');
+const replace = require('gulp-replace');
 
 // styles
 const scss = require('gulp-sass')(require('sass'));
@@ -7,48 +7,57 @@ const autoprefixer = require('gulp-autoprefixer');
 const cssMinify = require('gulp-clean-css');
 
 function styles() {
-    return src('./frontend/src/styles/**/*.scss')
-    .pipe( scss() )
-    .pipe( autoprefixer('last 2 versions') )
-    .pipe( cssMinify() )
-    .pipe( dest('./frontend/dist/styles/'))
+  return src('./frontend/src/styles/**/*.scss')
+    .pipe(scss())
+    .pipe(autoprefixer('last 2 versions'))
+    .pipe(cssMinify())
+    .pipe(dest('./frontend/dist/styles/'));
 }
-
 
 // scripts
 const jsMinify = require('gulp-terser');
 
 function scripts() {
-    return src('./frontend/src/scripts/**/*.js')
-    .pipe( jsMinify() )
-    .pipe( dest('./frontend/dist/scripts/'))
+  return src('./frontend/src/scripts/**/*.js')
+    .pipe(jsMinify())
+    .pipe(dest('./frontend/dist/scripts/'));
 }
 
+// Copy index.html to dist folder
+function copyHTML() {
+  return src('./frontend/index.html')
+    .pipe(replace('dist/styles/', 'styles/')) // Update style file paths
+    .pipe(replace('dist/scripts/', 'scripts/')) // Update script file paths
+    .pipe(dest('./frontend/dist/'));
+}
 
-
+// Copy and compress images
+function images() {
+    return src('./frontend/src/textures/**/*.{jpg,jpeg,png,gif,svg}')
+      .pipe(dest('./frontend/dist/textures/'));
+}
 
 // Server
 const browserSync = require('browser-sync').create();
 
-function browsersyncServer(cb){
-    browserSync.init({
-        server: {
-            baseDir: "./frontend"
-        }
-    });
-    cb();
+function browsersyncServer(cb) {
+  browserSync.init({
+    server: {
+      baseDir: './frontend'
+    }
+  });
+  cb();
 }
 
 function browserSyncReload(cb) {
-    browserSync.reload();
-    cb();
+  browserSync.reload();
+  cb();
 }
-
 
 function watchTask() {
-    watch('./frontend/*.html', browserSyncReload);
-    watch(['./frontend/src/styles/**/*.scss', './frontend/src/scripts/**/*.js'], series(styles, scripts, browserSyncReload));
+  watch('./frontend/*.html', series(copyHTML, browserSyncReload));
+  watch(['./frontend/src/styles/**/*.scss', './frontend/src/scripts/**/*.js'], series(styles, scripts, browserSyncReload));
+  watch('./frontend/src/textures/**/*.{jpg,jpeg,png,gif,svg}', series(images, browserSyncReload));
 }
 
-
-exports.default = series(styles, scripts, browsersyncServer, watchTask);
+exports.default = series(styles, scripts, copyHTML, images, browsersyncServer, watchTask);
