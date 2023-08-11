71ec4506ee01fbece78556b4835d5c10fc3f0412
joejamsky <joejamsky@gmail.com>
2023-08-11 00:19:31 -0400

    add orbit controls

--- VRshooter/frontend/src/scripts/app.js
+++ VRshooter/frontend/src/scripts/app.js
@@ -283,6 +283,15 @@ function initScene() {
     // </ 
 
 
+    const geometry = new THREE.PlaneGeometry( 10, 10 );
+    const material = new THREE.MeshBasicMaterial( {color: 0xffff00} );
+    const groundPlane = new THREE.Mesh( geometry, material );
+    groundPlane.position.set(0,0,0);
+    scene.add( groundPlane );
+
+
+
+
     // < Setup renderer
     renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true });
     renderer.setPixelRatio(window.devicePixelRatio);
